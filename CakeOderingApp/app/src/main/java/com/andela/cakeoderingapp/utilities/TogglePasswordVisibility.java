47c72897de03a8216fbff7bd408bc0adb2fe56d8
Joshua Ugba <justjosh001@gmail.com>
2016-01-27 21:46:17 +0100

    Work on sign in and sign out

--- CakeOderingApp/app/src/main/java/com/andela/cakeoderingapp/utilities/TogglePasswordVisibility.java
+++ CakeOderingApp/app/src/main/java/com/andela/cakeoderingapp/utilities/TogglePasswordVisibility.java
@@ -1,8 +1,12 @@
 package com.andela.cakeoderingapp.utilities;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.text.InputType;
 import android.util.AttributeSet;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageButton;
 
 import com.andela.cakeoderingapp.R;
@@ -21,21 +25,46 @@ public class TogglePasswordVisibility extends ImageButton {
     public TogglePasswordVisibility(Context context, AttributeSet set) {
         super(context,set);
 
-        //TypedArray attrs = context.getTheme().obtainStyledAttributes(set, R.styleable.TogglePasswordVisibilityButton, 0, 0);
+        TypedArray attrs = context.getTheme().obtainStyledAttributes(set, R.styleable.TogglePasswordVisibilityButton, 0, 0);
 
         try {
-//            passwordVisibleImageResource = attrs.getResourceId(R.styleable.TogglePasswordVisibilityButton_passwordVisibleImageResource, 0);
-//
-//            passwordInvisibleImageResource = attrs.getResourceId(R.styleable.TogglePasswordVisibilityButton_passwordInvisibleImageResource, 0);
-//
-//            passwordEditTextId = attrs.getResourceId(R.styleable.TogglePasswordVisibilityButton_passwordEditText, 0);
+            passwordVisibleImageResource = attrs.getResourceId(R.styleable.TogglePasswordVisibilityButton_passwordVisibleImageResource, 0);
+
+            passwordInvisibleImageResource = attrs.getResourceId(R.styleable.TogglePasswordVisibilityButton_passwordInvisibleImageResource, 0);
+
+            passwordEditTextId = attrs.getResourceId(R.styleable.TogglePasswordVisibilityButton_passwordEditText, 0);
 
             setImageResource(passwordInvisibleImageResource);
 
         } finally {
-            //attrs.recycle();
+            attrs.recycle();
         }
 
-        //attachOnToggleEventHandler();
+        attachOnToggleEventHandler();
+    }
+
+    public void attachOnToggleEventHandler() {
+        setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Activity activity = (Activity) getContext();
+
+                if (activity != null) {
+                    EditText passwordEditText = (EditText) activity.findViewById(passwordEditTextId);
+
+                    if (passwordEditText.getInputType() == (InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD)) {
+                        passwordEditText.setInputType(InputType.TYPE_CLASS_TEXT);
+                        setImageResource(passwordVisibleImageResource);
+
+                    } else {
+                        passwordEditText.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                        setImageResource(passwordInvisibleImageResource);
+                    }
+
+                    passwordEditText.setSelection(passwordEditText.getText().length());
+                }
+            }
+        });
+
     }
 }
